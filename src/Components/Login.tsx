6876e6dc59b1724c1052fc36e618b03a704830bd
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-07-23 02:09:15 +0600

    the form is ready

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -21,6 +21,11 @@ export class Login extends React.Component<LoginProps, LoginState> {
     return (
       <div>
         <h1>This is Login</h1>
+        <form>
+          <input type="text" value={this.state.userName} /> <br />
+          <input type="password" value={this.state.password} /> <br />
+          <input type="submit" value="Submit it" />
+        </form>
       </div>
     );
   }
